eb40130a559b18d9ebc17c69ec235f847a516df5
Ekaterina Koloyarskaya <KoloyarskayaEk@yandex.ru>
2026-06-08 10:37:09 +0300

    refactor: improve form code and fix styles

--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -8,6 +8,7 @@ import clsx from 'clsx';
 
 import styles from './ArticleParamsForm.module.scss';
 import { useOutsideClickClose } from 'src/ui/select/hooks/useOutsideClickClose';
+import { Separator } from 'src/ui/separator';
 
 type articleParamsFormProps = {
 	onApply: (newSettings: ArticleStateType) => void;
@@ -17,7 +18,7 @@ type articleParamsFormProps = {
 export const ArticleParamsForm = ({ onApply, onReset }: articleParamsFormProps) => {
 
 	const [isOpen, setIsOpen] = useState(false);
-	const rootRef = useRef<HTMLDivElement>(null);
+	const rootRef = useRef<HTMLElement>(null);
 
 	useOutsideClickClose({
 		isOpen,
@@ -29,35 +30,25 @@ export const ArticleParamsForm = ({ onApply, onReset }: articleParamsFormProps) 
 		setIsOpen(!isOpen)
 	}
 
-	const [selected, setSelected] = useState(defaultArticleState);
-	function handleFontFamilyChange(option: OptionType) {
-		setSelected({ ...selected, fontFamilyOption: option })
-	}
+	const [formState, setFormState] = useState(defaultArticleState);
 
-	function handleFontSizeOptions(option: OptionType) {
-		setSelected({ ...selected, fontSizeOption: option })
-	}
-
-	function handleFontColors(option: OptionType) {
-		setSelected({ ...selected, fontColor: option })
-	}
-
-	function handleBackgroundColors(option: OptionType) {
-		setSelected({ ...selected, backgroundColor: option })
-	}
-
-	function handleContentWidthArr(option: OptionType) {
-		setSelected({ ...selected, contentWidth: option })
-	}
+	const updateFormField = (field: keyof ArticleStateType) => {
+		return (value: OptionType) => {
+			setFormState((prev) => ({
+				...prev,
+				[field]: value,
+			}));
+		};
+	};
 
 	function handleSubmit(e: React.FormEvent) {
 		e.preventDefault();
-		onApply(selected);
+		onApply(formState);
 	}
 
 	function handleReset(e: React.FormEvent) {
 		e.preventDefault();
-		setSelected(defaultArticleState);
+		setFormState(defaultArticleState);
 		onReset(defaultArticleState);
 	}
 
@@ -66,11 +57,12 @@ export const ArticleParamsForm = ({ onApply, onReset }: articleParamsFormProps) 
 			<ArrowButton isOpen={isOpen} onClick={handleIsOpenForm} />
 			<aside ref={rootRef} className={clsx(styles.container, isOpen && styles.container_open)}>
 				<form className={styles.form} onSubmit={handleSubmit} onReset={handleReset}>
-					<Select title='Шрифт' options={fontFamilyOptions} selected={selected.fontFamilyOption} onChange={handleFontFamilyChange} />
-					<RadioGroup title='Размер шрифта' name='font-size' options={fontSizeOptions} selected={selected.fontSizeOption} onChange={handleFontSizeOptions} />
-					<Select title='Цвет шрифта' options={fontColors} selected={selected.fontColor} onChange={handleFontColors} />
-					<Select title='Цвет фона' options={backgroundColors} selected={selected.backgroundColor} onChange={handleBackgroundColors} />
-					<Select title='Ширина контента' options={contentWidthArr} selected={selected.contentWidth} onChange={handleContentWidthArr} />
+					<Select title='Шрифт' options={fontFamilyOptions} selected={formState.fontFamilyOption} onChange={updateFormField('fontFamilyOption')} />
+					<RadioGroup title='Размер шрифта' name='font-size' options={fontSizeOptions} selected={formState.fontSizeOption} onChange={updateFormField('fontSizeOption')} />
+					<Select title='Цвет шрифта' options={fontColors} selected={formState.fontColor} onChange={updateFormField('fontColor')} />
+					<Separator />
+					<Select title='Цвет фона' options={backgroundColors} selected={formState.backgroundColor} onChange={updateFormField('backgroundColor')} />
+					<Select title='Ширина контента' options={contentWidthArr} selected={formState.contentWidth} onChange={updateFormField('contentWidth')} />
 					<div className={styles.bottomContainer}>
 						<Button title='Сбросить' htmlType='reset' type='clear' />
 						<Button title='Применить' htmlType='submit' type='apply' />
